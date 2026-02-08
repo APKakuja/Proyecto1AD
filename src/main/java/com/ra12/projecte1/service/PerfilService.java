bd9d290e3cd71ec6268959bfeddd6baf57908651
iaas22 <ishaaamin22@gmail.com>
2026-02-08 18:17:37 +0100

    mergeando cambios

--- src/main/java/com/ra12/projecte1/service/PerfilService.java
+++ src/main/java/com/ra12/projecte1/service/PerfilService.java
@@ -82,23 +82,6 @@ public class PerfilService {
         CustomLogging.info("PerfilService", "importarCSV", "CSV importado. Registros=" + count);
     }
 
-    public void guardarImagen(int id, MultipartFile file) throws IOException {
-        CustomLogging.info("PerfilService", "guardarImagen", "Subiendo imagen para id=" + id);
-
-        String uploadDir = System.getProperty("user.dir") + "/uploads/";
-        File carpeta = new File(uploadDir);
-        if (!carpeta.exists()) {
-            carpeta.mkdirs();
-        }
-
-        String nombreArchivo = id + "_" + file.getOriginalFilename().replaceAll(" ", "_");
-        File destino = new File(uploadDir + nombreArchivo);
-        file.transferTo(destino);
-
-        repository.guardarImagen(id, nombreArchivo);
-
-        CustomLogging.info("PerfilService", "guardarImagen", "Imagen guardada correctamente: " + nombreArchivo);
-    }
 
     public void actualizarPerfil(int id, Perfil perfil) {
         CustomLogging.info("PerfilService", "actualizarPerfil", "Actualizando perfil id=" + id);
@@ -116,7 +99,6 @@ public class PerfilService {
     }
 
 
-    //Subir una imagen
     public void guardarImagen(int id, MultipartFile file) throws IOException {
         String uploadDir = System.getProperty("user.dir") + "/uploads/";
         File carpeta = new File(uploadDir);
@@ -132,18 +114,4 @@ public class PerfilService {
     }
 
 
-    // Actualizar perfil por id
-    public void actualizarPerfil(int id, Perfil perfil) {
-        repository.actualizarPerfil(id, perfil);
-    }
-
-    // Eliminar un perfil por id
-    public void eliminarPorId(int id) {
-        repository.eliminarPorId(id);
-    }
-
-    // Eliminar todos los perfiles
-    public void eliminarTodos() {
-        repository.eliminarTodos();
-    }
 }
